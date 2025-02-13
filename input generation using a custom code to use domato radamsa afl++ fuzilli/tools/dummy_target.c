cce18c12d6d4128803dda9375ef494fff33bdc22
Dhia <dhia@dissertation.c.ecstatic-antler-449501-g6.internal>
2025-02-13 18:15:02 +0000

    collecting all the testing seeds in one folder with the lastest update of generation

---
+++ input generation using a custom code to use domato radamsa afl++ fuzilli/tools/dummy_target.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // Optionally, you could read the input file, but here we ignore it.
+    // Just print a message (or do nothing) and exit with success.
+    printf("Dummy target executed.\n");
+    return 0;
+}
